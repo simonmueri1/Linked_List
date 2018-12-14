4f43f0e4d2543e8f097326efb0fb67f6449ef3e7
milei02 <45421134+milei02@users.noreply.github.com>
2018-12-14 13:32:59 +0100

    Create Menu

--- Linked_List/Quelle.cpp
+++ Linked_List/Quelle.cpp
@@ -1,2 +1,32 @@
 #include "stdio.h"
 #include "stdlib.h"
+
+void sequences();
+void printMenu();
+
+void main() {
+	sequences();
+}
+
+
+void sequences() {
+	printMenu();
+}
+
+void printMenu() {
+	printf("_________________________________________________________________\n");
+	printf("LINKED LIST - APP from Simon Mueri & Michel Leisibach v1.0\n");
+	printf("_________________________________________________________________\n");
+	printf("Create List\t\t = [C]\n");
+	printf("Delete List\t\t = [DE]\n");
+	printf("Print List Elements\t\ = [P] + (Number)\n");
+	printf("Print all\t\t = [P] +fsadf (0)\n");
+	printf("Delete Element\t\t = [DE] + (Firstname + Lastname)\n");
+	printf("End Program\t\t = [X]\n");
+	printf("_________________________________________________________________\n\n");
+	
+	system("pause");
+
+}
+
+
