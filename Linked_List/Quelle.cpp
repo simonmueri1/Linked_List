4fc30dcdde543a6dba536e0cdeb65d1793bfa789
milei02 <45421134+milei02@users.noreply.github.com>
2018-12-14 13:43:22 +0100

    Create input & base

--- Linked_List/Quelle.cpp
+++ Linked_List/Quelle.cpp
@@ -3,6 +3,7 @@
 
 void sequences();
 void printMenu();
+void scanInput();
 
 void main() {
 	sequences();
@@ -10,23 +11,41 @@ void main() {
 
 
 void sequences() {
+	char input[30];
+
 	printMenu();
+	input = scanInput();
+	system("pause");
+
+	switch(input)
+		case C:
+				break;
+		case DE: 
+				break;
+		case P:
+				break;
 }
 
 void printMenu() {
 	printf("_________________________________________________________________\n");
 	printf("LINKED LIST - APP from Simon Mueri & Michel Leisibach v1.0\n");
 	printf("_________________________________________________________________\n");
-	printf("Create List\t\t = [C]\n");
-	printf("Delete List\t\t = [DE]\n");
-	printf("Print List Elements\t\ = [P] + (Number)\n");
-	printf("Print all\t\t = [P] +fsadf (0)\n");
+	printf("Create List\t\t = [CL]\n");
+	printf("Delete List\t\t = [DL]\n");
+	printf("Print List Elements\t\ = [PE] + (Number)\n");
+	printf("Print all\t\t = [PE] + (0)\n");
 	printf("Delete Element\t\t = [DE] + (Firstname + Lastname)\n");
-	printf("End Program\t\t = [X]\n");
+	printf("End Program\t\t = [XX]\n");
 	printf("_________________________________________________________________\n\n");
-	
-	system("pause");
+}
 
+void scanInput() {
+	char input[30];
+	scanf_s(input);
+
+	if(input)
+
+	return input;
 }
 
 
