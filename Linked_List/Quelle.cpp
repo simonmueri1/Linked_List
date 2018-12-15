ccb85bf4e977115c763cd5c0359a0abb23bb1fce
milei02 <45421134+milei02@users.noreply.github.com>
2018-12-15 10:01:30 +0100

    Check input

--- Linked_List/Quelle.cpp
+++ Linked_List/Quelle.cpp
@@ -3,27 +3,40 @@
 
 void sequences();
 void printMenu();
-void scanInput();
+void scanChar();
+struPerson* createList(int Anzahl)
 
 void main() {
 	sequences();
 }
 
+typedef struct Person {
+	char Vorname[40];
+	char Nachname[40];
+	int Jahrgang;
+	struct Person* pNext;
+} struPerson;
 
 void sequences() {
 	char input[30];
 
 	printMenu();
-	input = scanInput();
-	system("pause");
+	input = scanChar();
 
-	switch(input)
-		case C:
+	switch (input)
+		case CL:	
+					createList();
+					break;
+		case DL: 
 				break;
-		case DE: 
+		case PE:
 				break;
-		case P:
+		case DE:
 				break;
+		case XX:
+				break;
+
+	system("pause");
 }
 
 void printMenu() {
@@ -32,20 +45,52 @@ void printMenu() {
 	printf("_________________________________________________________________\n");
 	printf("Create List\t\t = [CL]\n");
 	printf("Delete List\t\t = [DL]\n");
-	printf("Print List Elements\t\ = [PE] + (Number)\n");
-	printf("Print all\t\t = [PE] + (0)\n");
-	printf("Delete Element\t\t = [DE] + (Firstname + Lastname)\n");
+	printf("Print List Elements\t\ = [PE]\n");
+	printf("Print all\t\t = [PE0]\n");
+	printf("Delete Element\t\t = [DE]\n");
 	printf("End Program\t\t = [XX]\n");
 	printf("_________________________________________________________________\n\n");
 }
 
-void scanInput() {
-	char input[30];
+char scanChar() {
+	char input[];
 	scanf_s(input);
 
-	if(input)
+	//start programm new if input is not valid.
+	if (input != "CL" || input != "DL" || input != "PE" || input != "PE0" || input != "DE" || input != "XX") {
+		printf("Please enter a valid entry.");
+		system("cls");
+		main();
+	}
 
 	return input;
 }
 
+int scanNumber(){
+	int number = 0;
+	printf("Enter a number.");
+	scanf_s(number);
 
+	if (number != 0..9) {
+		printf("Please enter a vaild number.\n")
+			scanNumber();
+	}														
+
+	return number;
+
+
+struPerson* createList(int Anzahl) {
+	// Erstellt eine Liste von definierter Anzahl Elementen.
+	struPerson* pStart = NULL;
+	struPerson* pLast = NULL;
+
+	for (int iElm = 0; iElm < Anzahl; iElm) {
+		struPerson* pNew = (struPerson*)malloc(sizeof(struPerson));
+		pNew->Nummer = iElm + 1;
+		pNew->pNext = NULL;
+		if (pStart == NULL) pStart = pNew;
+		if (pLast != NULL) pLast->pNext = pNew;
+		pLast = pNew;
+	}
+	return pStart;
+}
